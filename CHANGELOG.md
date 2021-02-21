94c68fe07264bcf53ba865b29902314ec8c09ec1
ucwaldo <theo.fie@gmail.com>
2021-02-21 09:08:41 +0100

    Version release 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 <!-- %% CHANGELOG_ENTRIES %% -->
 
+## 1.2.0 - 21. Feb 2021 08:08
+
+Add semver test small cleanup
+
+
 ## 1.1.0 - 21. Feb 2021 01:13
 
 Command line options
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Expublish.MixProject do
   def project do
     [
       app: :expublish,
-      version: "1.1.0",
+      version: "1.2.0",
       package: package(),
       description: description(),
       source_url: "https://github.com/ucwaldo/expublish",
