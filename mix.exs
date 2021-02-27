dd1ec1fb0affbe084fe21521397a41b57349a909
ucwaldo <ucwaldo@users.noreply.github.com>
2021-02-27 13:41:07 +0000

    Version release 2.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Expublish.MixProject do
   def project do
     [
       app: :expublish,
-      version: "2.3.4",
+      version: "2.4.0",
       package: package(),
       description: description(),
       source_url: "https://github.com/ucwaldo/expublish",
